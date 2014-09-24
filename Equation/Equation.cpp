0be44b75d44bc2819bd62233bdcf065f25ce83cb
ilyavelilyaev <it4l@icloud.com>
2014-09-24 15:26:57 +0400

    Deleted unnecessary spaces

--- Equation/Equation.cpp
+++ Equation/Equation.cpp
@@ -9,19 +9,14 @@
 #include "Equation.h"
 
 ERR_ENUM_TYPE solve1or2power (double *inp, double *answers, int length) {
-   
-    if ((inp[0] == 0) && (inp[1] == 0))
-    {
-        return NOT_AN_EQUATION;
-    } else {
-        if (inp[0] == 0)
-        {
+    if ((inp[0] == 0) && (inp[1] == 0)) return NOT_AN_EQUATION;
+    else {
+        if (inp[0] == 0) {
             answers[0] = 1;
             answers[1] = -inp[2]/inp[1];
         } else {
             double discriminant = inp[1] * inp[1] - 4 * inp[0] * inp [2];
-            if (discriminant >= 0)
-            {
+            if (discriminant >= 0) {
                 answers[0] = 2;
                 answers[1] = (-inp[1] + sqrt(discriminant)) / (2 * inp[0]);
                 answers[2] = (-inp[1] - sqrt(discriminant)) / (2 * inp[0]);
@@ -34,23 +29,14 @@ ERR_ENUM_TYPE solve1or2power (double *inp, double *answers, int length) {
 }
 
 ERR_ENUM_TYPE solve (double *inp, double *answers, int length) {
-    
-    
-    if (POWER_OF_EQUATION > 2)
-    {
-        
-        return FUNCTION_NOT_IMPLEMENTED;
-        
-    } else {
-        return solve1or2power(inp, answers, length);
-    }
+    if (POWER_OF_EQUATION > 2) return FUNCTION_NOT_IMPLEMENTED;
+    else return solve1or2power(inp, answers, length);
     return NO_ERROR;
 }
 
 void cut_string (char *string) {
     char *strtemp = strchr(string, 32);
     if (strtemp) {
-        
         for (int i = 0; i < strlen(strtemp) - 1; i++) {
             string[i] = strtemp[i + 1];
         }
@@ -58,13 +44,10 @@ void cut_string (char *string) {
 }
 
 void convert (char *string, double *inp, int length, unsigned long string_length) {
-    
-    
     for (int i = 0; i < length; i++) {
         sscanf(string, "%lg", &inp[i]);
         cut_string(string);
     }
-    
 }
 
 ERR_ENUM_TYPE  delete_unwanted_spaces_check_convert (char *ENTERED_STRING, double *inp, int length) {
@@ -76,11 +59,14 @@ ERR_ENUM_TYPE  delete_unwanted_spaces_check_convert (char *ENTERED_STRING, doubl
             k++;
         }
     }
+    
     k--;
+    
     while (strtemp[k] == ' ') {
         strtemp[k] = 0;
         k--;
     }
+    
     ENTERED_STRING = strtemp;
     
     int COUNT_OF_ENTERED_FACTORS = 1;
@@ -90,8 +76,6 @@ ERR_ENUM_TYPE  delete_unwanted_spaces_check_convert (char *ENTERED_STRING, doubl
         
         if (ENTERED_STRING[i] == 32) COUNT_OF_ENTERED_FACTORS ++;
     }
-    
-    // if (ENTERED_STRING[strlen(ENTERED_STRING) - 2] == ' ') COUNT_OF_ENTERED_FACTORS --;
     
     if (COUNT_OF_ENTERED_FACTORS != length) return NOT_EXACT_NUMBER_OF_FACTORS_ENTERED;
     
@@ -110,43 +94,29 @@ ERR_ENUM_TYPE input (double *inp, int length) {
     fgets (temp2, 256, stdin);
     strcpy(ENTERED_STRING, temp);
     strcat(ENTERED_STRING, temp2);
-    
-    
-    
+
     return delete_unwanted_spaces_check_convert(ENTERED_STRING, inp, length);
-    
-    
-    
 }
 
 void output (double *answers, int length) {
-    if (answers[0] != 0)
-    {
+    if (answers[0] != 0) {
         if (answers[1] == 0) answers[1] = 0;
         printf("%lg\n", answers[1]);
-        for (int i = 2; i < answers[0] + 1; i++)
-        {
-            if (answers[i] != answers[i - 1])
-            {
+        for (int i = 2; i < answers[0] + 1; i++) {
+            if (answers[i] != answers[i - 1]) {
                 if (answers[i] == 0) answers[i] = 0;
                 printf("%lg\n", answers[i]);
             }
-            
         }
     }
 }
 
 ERR_ENUM_TYPE sort (double *answers, int length) {
     double *answersSorted = answers;
-    
-    if (answers[0] > 1)
-    {
-        for(int x = 1; x < answers[0] + 1; x++)
-        {
-            for(int y=1; y < answers[0]; y++)
-            {
-                if(answersSorted[y]>=answersSorted[y+1])
-                {
+    if (answers[0] > 1) {
+        for(int x = 1; x < answers[0] + 1; x++) {
+            for(int y=1; y < answers[0]; y++) {
+                if(answersSorted[y]>=answersSorted[y+1]) {
                     double temp = answersSorted[y+1];
                     answersSorted[y+1] = answersSorted[y];
                     answersSorted[y] = temp;
@@ -231,7 +201,6 @@ int main() {
     double answers[len];
     
     while (WILL_REPEAT) {
-        
         error = run(inp, answers, len);
         
         if (error != NO_ERROR) {
